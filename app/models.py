34121d910eb326f2aa503f0cee660004eb0bdba1
Antoine <saruelucas2@gmail.com>
2025-12-16 13:59:11 +0100

    ménage

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from app.schemas.items import Base
+from app.services.business_logic import Auteur, Livre
+
 
 # Créer la base de données
 engine = create_engine("sqlite:///bibliotheque.db")
