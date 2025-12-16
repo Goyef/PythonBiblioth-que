0baf33fcd3b24ae4ac8c17d9b4c0cdd7976472ca
Antoine <saruelucas2@gmail.com>
2025-12-16 11:28:27 +0100

    implémentation route api de test

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI(
+    title="Bibliothèque API",
+    description="API pour gérer une bibliothèque de livres.",
+    version="1.0.0"
+)
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenue à l'API de la Bibliothèque"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"item": item, "message": "Item créé avec succès"}
