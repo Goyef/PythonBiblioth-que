78613f3ac67e347c723a8d6124afbaffea83697f
MathKasp <math.kasperczak@icloud.com>
2025-12-18 10:05:40 +0100

    fix() résolution de pb CRUD FONCTIONNE AUTEUR

--- app/routers/author.py
+++ app/routers/author.py
@@ -1,78 +1,71 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.models import Session as SessionLocal, Auteur
+from app.models import Author as Auteur
+from app.database import get_session
 
 router = APIRouter(
-    prefix="/auteurs",
-    tags=["auteurs"]
+    prefix="/authors",
+    tags=["authors"]
 )
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@router.get("/auteurs")
-def get_auteurs(page: int = 1, db: Session = Depends(get_db)):
+@router.get("/authors")
+def get_authors(page: int = 1, db: Session = Depends(get_session)):
     per_page = 5
     offset = (page - 1) * per_page
-    auteurs = db.query(Auteur).offset(offset).limit(per_page).all()
+    authors = db.query(Auteur).offset(offset).limit(per_page).all()
     # nom = db.query(Auteur).offset(offset).limit(per_page).all()
     # prenom = db.query(Auteur).offset(offset).limit(per_page).all()
     # date_naissance = db.query(Auteur).offset(offset).limit(per_page).all()
     # date_deces = db.query(Auteur).offset(offset).limit(per_page).all()
     return {
-        "auteurs": [{"id": a.id, "nom": a.nom, "prenom": a.prenom, "date_naissance": a.date_naissance, "date_deces": a.date_deces} for a in auteurs],
+        "authors": [{"id": a.id, "nom": a.last_name, "prenom": a.first_name, "date_naissance": a.birthdate, "date_deces": a.death_date} for a in authors],
         "page": page,
         "total": db.query(Auteur).count(),
         "pages": (db.query(Auteur).count() + per_page - 1)
     }
 
-@router.delete("/delete/{auteur_id}")
-def delete_auteur(auteur_id: int, db: Session = Depends(get_db)):
-    auteur = db.query(Auteur).filter(Auteur.id == auteur_id).first()
-    if not auteur:
+@router.delete("/delete/{author_id}")
+def delete_author(author_id: int, db: Session = Depends(get_session)):
+    author = db.query(Auteur).filter(Auteur.id == author_id).first()
+    if not author:
         raise HTTPException(status_code=404, detail="Auteur non trouvé")
-    db.delete(auteur)
+    db.delete(author)
     db.commit()
-    return {"message": f"Auteur {auteur_id} supprimé"}
-
+    return {"message": f"Auteur {author_id} supprimé"}
 @router.post("/add")
-def ajouter_auteur(nom: str, prenom: str, biographie: str, nationalite: str, date_naissance: str, date_deces: str | None = None, db: Session = Depends(get_db)):
+def ajouter_auteur(last_name: str, first_name: str, biographie: str, nationalite: str, date_naissance: str, date_deces: str | None = None, db: Session = Depends(get_session)):
     new_auteur = Auteur(
-        nom=nom,
-        prenom=prenom,
+        last_name=last_name,
+        first_name=first_name,
         biographie=biographie,
         nationalite=nationalite,
-        date_naissance=date_naissance,
-        date_deces=date_deces
+        birthdate=date_naissance,
+        death_date=date_deces
     )
     db.add(new_auteur)
     db.commit()
     db.refresh(new_auteur)
     return {"message": "Auteur ajouté avec succès", "auteur_id": new_auteur.id}
 
-@router.put("/update/{auteur_id}")
-def update_auteur(auteur_id: int, nom: str | None = None, prenom: str | None = None, biographie: str | None = None, nationalite: str | None = None, date_naissance: str | None = None, date_deces: str | None = None, db: Session = Depends(get_db)):
-    auteur = db.query(Auteur).filter(Auteur.id == auteur_id).first()
-    if not auteur:
+@router.put("/update/{author_id}")
+def update_auteur(author_id: int, last_name: str | None = None, first_name: str | None = None, biographie: str | None = None, nationalite: str | None = None, birthdate: str | None = None, death_date: str | None = None, db: Session = Depends(get_session)):
+    author = db.query(Auteur).filter(Auteur.id == author_id).first()
+    if not author:
         raise HTTPException(status_code=404, detail="Auteur non trouvé")
-    
-    if nom is not None:
-        auteur.nom = nom
-    if prenom is not None:
-        auteur.prenom = prenom
+
+    if last_name is not None:
+        author.last_name = last_name
+    if first_name is not None:
+        author.first_name = first_name
     if biographie is not None:
-        auteur.biographie = biographie
+        author .biographie = biographie
     if nationalite is not None:
-        auteur.nationalite = nationalite
-    if date_naissance is not None:
-        auteur.date_naissance = date_naissance
-    if date_deces is not None:
-        auteur.date_deces = date_deces
-    
+        author.nationalite = nationalite
+    if birthdate is not None:
+        author.birthdate = birthdate
+    if death_date is not None:
+        author.death_date = death_date
+
     db.commit()
-    db.refresh(auteur)
-    return {"message": "Auteur mis à jour avec succès", "auteur_id": auteur.id}
+    db.refresh(author)
+    return {"message": "Auteur mis à jour avec succès", "auteur_id": author.id}
